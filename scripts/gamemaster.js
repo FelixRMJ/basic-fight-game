17ef6ff637d4e6b74bbf43175cc8f52f7d5b98c0
felix rodriguezmanzaneque <felixrmj@gmail.com>
2020-10-16 16:52:29 +0200

    created the function to call classType from the HTML and create the player when choosing an image

--- scripts/gamemaster.js
+++ scripts/gamemaster.js
@@ -1,3 +1,30 @@
 let gameMaster = {
   //Create a Method that is going to create the info that is going to be assigned to the Player object constructor
+
+  setGameStart: function(classType){
+    //run 2 methods inside this object to run it on the onclick html event
+    this.resetPlayer(classType);//we bring here the string from the onclick event on the HTML
+    this.setPreFight();
+  },
+  //create the player inside the game
+  resetPlayer: function(classType){
+    switch(classType){
+      case 'Simp':
+        player = new Player(classType, 50, 0, 30, 200, 100, 9);
+        break;
+      case 'DickMan':
+        player = new Player(classType, 50, 0, 30, 200, 100, 9);
+        break;
+      case 'PussyMonster':
+        player = new Player(classType, 50, 0, 30, 200, 100, 9);
+        break;
+      case 'Example':
+        player = new Player(classType, 50, 0, 30, 200, 100, 9);
+        break;
+   }
+  },
+  //sets the first stage inside the game
+  setPreFight: function(){
+
+  }
 }
