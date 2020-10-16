dbf10cd69ee8e608f695f44a94f64fc096d4f68b
felix rodriguezmanzaneque <felixrmj@gmail.com>
2020-10-16 16:32:53 +0200

    added a bit of css and the enemy object constructor

---
+++ scripts/enemy.js
@@ -0,0 +1,11 @@
+let enemy;
+
+function Player(enemyType, health, mana, strength, agility, speed, moisture){
+  this.enemyType = enemyType;
+  this.health = health;
+  this.mana = mana;
+  this.strength = strength;
+  this.agility = agility;
+  this.speed = speed;
+  this.moisture = moisture;
+}
--- scripts/player.js
+++ scripts/player.js
@@ -1,3 +1,6 @@
+// Declare the player variable in the global scope so it's always available
+let player;
+
 //Player object constructor
 function Player(classType, health, mana, strength, agility, speed, moisture){
   this.classType = classType;
