3aad64a7e57467d84c95bc9ba2ab9100aaa6b833
felix rodriguezmanzaneque <felixrmj@gmail.com>
2020-10-16 16:22:55 +0200

    created object constructor

---
+++ scripts/gamemaster.js
@@ -0,0 +1,3 @@
+let gameMaster = {
+  //Create a Method that is going to create the info that is going to be assigned to the Player object constructor
+}
---
+++ scripts/player.js
@@ -0,0 +1,10 @@
+//Player object constructor
+function Player(classType, health, mana, strength, agility, speed, moisture){
+  this.classType = classType;
+  this.health = health;
+  this.mana = mana;
+  this.strength = strength;
+  this.agility = agility;
+  this.speed = speed;
+  this.moisture = moisture;
+}
